33bda35b231e57bdbc78bff15d3fa4100aa565b8
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-11-25 18:39:44 +0530

    Express Js

--- express/CA1_Revision/08_express.js
+++ express/CA1_Revision/08_express.js
@@ -1,9 +1,18 @@
 import express from 'express'
+import Users from './08_structuringRoutes.js'
+
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+// Mounting the router on different paths
+app.use("/users", Users) // Mounting the router on /users path
+
+app.use("/home", Users); // Mounting the router on /home path
+app.use("/about", Users); // Mounting the router on /about path
+
+// Defining other routes directly in the main app
 app.get('/', (req, res) => {
     res.send("Hello from Express ");
 })
