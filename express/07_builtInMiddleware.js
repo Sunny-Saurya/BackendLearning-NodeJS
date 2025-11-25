0eebacf2b060e388e5361e84818a8176b5146d80
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-11-26 00:07:47 +0530

    Build in MiddleWare

--- express/07_builtInMiddleware.js
+++ express/07_builtInMiddleware.js
@@ -3,21 +3,18 @@ import express from 'express';
 import cookie from 'cookie-parser'
 const app = express();
 app.use(cookie())
-// setting the cookie
+
 app.get('/set', (req, res) => {
     res.cookie('coursecode', 'INT222', {maxAge:15000});
     res.send("Cookies Set !!")
 })
 
-// fetching the cookioe
 app.get('/fetch', (req, res) => {
     console.log(req.cookies);
     res.send( "Cooking Fetched !!"
     )
-    
 })
 
-// clearing the cookie
 app.get('/clear', (req, res) => {
     res.clearCookie('coursecode')
     res.send("The cookie is deleted !!")
