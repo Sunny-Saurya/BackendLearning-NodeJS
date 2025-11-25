a8a3fc60aaee27f6e14f86045656ff2acd85b06f
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-11-26 00:07:27 +0530

    Middleware

--- express/04_middleware.js
+++ express/04_middleware.js
@@ -6,17 +6,19 @@ const myLogger = (req, res, next) => {
     console.log("After logging");
     next();
 }
-// app.use(myLogger);
+
 app.get("/", (req, res) => {
     res.send(`<h4>Welcome Back ! Sunny</h4>`)
     console.log("Welcome Sunny !!");
     res.end();
 })
-app.get("/home", myLogger, (req, res) => {  // if you want to use middleware for specific route
+
+app.get("/home", myLogger, (req, res) => { 
     res.send(`<h4>Welcome to Hoem Page Sunny</h4>`)
     console.log("Welcome Home Sunny !!");
     res.end();
 })
+
 app.get("/about", (req, res) => {
     res.send(`<h4>Welcome to About Page ! Sunny</h4>`)
     console.log("Welcome About Sunny !!");
