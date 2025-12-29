b191acc2620bf2e92688b1ba8684c62094b132fe
Sunny_Saurya <sunnysaurya9955@gmail.com>
2025-12-30 01:35:16 +0530

    Playing with JSON

--- express/ETP/02_Json.js
+++ express/ETP/02_Json.js
@@ -8,8 +8,24 @@ const user = {
 const jsonData = JSON.stringify(user);
 console.log(jsonData);
 
+// converting json to js object
+
+const jsData = JSON.parse(jsonData);
+console.log(jsData);
+
+
 fs.writeFile('02_json.txt', jsonData, (err) => {
     if(err) console.log("Error Writing");
     else console.log("Successfully Written !!");
     
+})
+
+// reading from json file
+
+fs.readFile('02_json.txt', 'utf8', (err, data) => {
+    if(err) console.log("Error Reading!!");
+    else {
+        console.log("Successfully Read!!");
+        console.log(data);
+    }
 })
